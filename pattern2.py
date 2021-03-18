6f667017ee378f66b07d78d584e8c04632547dbc
Deepak Parihar <80037185+deepakparihar10@users.noreply.github.com>
2021-03-18 23:59:13 +0530

    Add files via upload

---
+++ pattern2.py
@@ -0,0 +1,7 @@
+n=eval(input('Enter the number of lines'))
+ch=65
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(ch),end=" ")
+        ch+=1
+    print()
---
+++ pattern4.py
@@ -0,0 +1,3 @@
+n=eval(input('Enter the number of lines'))
+for i in range(1,n+1):
+    print(' '*(n-i)+'*'*i)
